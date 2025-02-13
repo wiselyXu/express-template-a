f27420417f7051e84af0f30bbf6f98953606b648
xugang <twtqmq1@163.com>
2025-02-13 22:12:39 +0800

    support CORS and  morgan

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const  express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 
+
+
 const PORT = process.env.port || 3000
 
-// 
+// support Cross original     支持跨域
+app.use(cors())
+// enalbe http request log
+app.use(morgan('dev'))
+app.use(express.json())  // 接收 json格式的请求参数
+app.use(express.urlencoded())  //接收url encoded的参数
+
 app.use('/api',router)
 
-app.use(errorHandler())
+//app.use(errorHandler())
 
 app.listen(PORT,() =>{
-    console.log(`Server is runing at http://localhost:${PORT}`)
+    
+    console.log(`Server is runing at http://localhost:${PORT} `)
+    console.log('CORS-enabled for all request: cofigure more cors, \n'
+        + 'please refer  : https://expressjs.com/en/resources/middleware/cors.html ')
 })
 
 
